09e31d244b75ea016546efc5667d8773cd6bb921
ashwani kumar <ashwani.kumar7@oyorooms.com>
2019-12-08 10:00:58 +0530

    design

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
       <div className="App">
         {/* <Container> */}
         <Router>
-          <Navbar bg="dark" expand="lg">
-            <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="/home">BookMyFlight</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
