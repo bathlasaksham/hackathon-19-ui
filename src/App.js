29d990524d7c04713106c71c54882fc7a080c477
Ayush Singla <ayush.singla@oyorooms.com>
2019-12-08 09:23:47 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Home from './Home';
+import Track from './Track';
+import Transactions from './Transactions';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Container> */}
+        <Router>
+          <Navbar bg="dark" expand="lg">
+            <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/home">Book</Nav.Link>
+                <Nav.Link href="/transactions">Transactions</Nav.Link>
+                <Nav.Link href="/track-luggage">Track</Nav.Link>
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown> */}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/transactions" component={Transactions} />
+          <Route exact path="/track-luggage" component={Track} />
+        </Router>
+        {/* </Container> */}
+      </div>
+    );
+  }
+}
+
+export default App;
